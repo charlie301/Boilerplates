1e920f2c5bde65ea7e8209879fa04d9e8b7d64d7
charlie <charliestorey103@gmail.com>
2016-11-21 18:33:34 -0500

    ADD: R/R added to boilerplate projects

---
+++ Nov-React-Redux-SASS-Boilerplate/src/js/app.jsx
@@ -0,0 +1,8 @@
+//Base React imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Import Test componentwsdads
+import TestComponent from './components/TestComponent';
+
+ReactDOM.render(<TestComponent />, document.getElementById('container'));
